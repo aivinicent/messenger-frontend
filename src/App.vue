3575f0f57291f673a713de634ca411db598314c2
Владислав Калмыков <vakalmykov@eyevox.ru>
2021-12-20 23:17:51 +0300

    project init, get messages, show messages

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <Messages/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Messages from './components/Messages';
+
+export default {
+  components: {
+    Messages,
+  },
+
+  data: () => ({}),
+};
+</script>
---
+++ src/components/Messages.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-card flat>
+      <v-list dense>
+        <v-list-item v-for="message in messages" :key="message.id">
+          <v-list-item-content>
+            <v-card outlined>
+              <v-card-text class="d-flex justify-space-between">
+                <span>{{ "#" + message.id }}</span>
+                <span>{{ message.timestamp }}</span>
+              </v-card-text>
+              <v-card-title>{{ message.body }}</v-card-title>
+            </v-card>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    messages: [],
+  }),
+
+  created() {
+    fetch("http://localhost:8000/messages")
+      .then((response) => response.json())
+      .then((data) => (this.messages = data));
+  },
+};
+</script>
